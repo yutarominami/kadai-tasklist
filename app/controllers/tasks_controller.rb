5b51593d09f38d52ca94894e95d47c2deae1f1d6
jiro.kirameki <yutarominmi@yahoo.co.jp>
2017-04-25 00:56:33 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,6 @@ class TasksController < ApplicationController
   end
    
   def task_params
-    params.require(:task).permit(:content,:status)
+    params.require(:task).permit(:content,　:status)
   end
 end
--- db/migrate/20170424065407_add_status_to_tasks.rb
+++ db/migrate/20170424065407_add_status_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddstatusToTasks < ActiveRecord::Migration[5.0]
+class AddStatusToTasks < ActiveRecord::Migration[5.0]
   def change
     add_column :tasks, :status, :string
   end
